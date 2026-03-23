371f6360c0ad4470dbe94586ae08309aabceb815
Bipin Kunjumon <151365185+bipinkunjumon1@users.noreply.github.com>
2026-03-23 08:15:34 +0530

    Add files via upload

---
+++ backend/Program.cs
@@ -0,0 +1,44 @@
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication8.Contracts;
+using WebApplication8.service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<Iuserservice, userservice>();
+builder.Services.AddScoped<Ipartservice, partservice>();
+builder.Services.AddScoped<IDbConnection>(sp=>
+    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")
+));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
